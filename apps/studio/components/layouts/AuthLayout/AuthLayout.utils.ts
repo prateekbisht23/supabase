877d1a110223ca360462bb5bc441635c61b8b63a
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-09-08 19:57:18 +0200

    oAuth apps crud

--- apps/studio/components/layouts/AuthLayout/AuthLayout.utils.ts
+++ apps/studio/components/layouts/AuthLayout/AuthLayout.utils.ts
@@ -27,9 +27,9 @@ export const generateAuthMenu = (
       items: [
         { name: 'Users', key: 'users', url: `/project/${ref}/auth/users`, items: [] },
         {
-          name: 'OAuth Clients',
-          key: 'oauth-clients',
-          url: `/project/${ref}/auth/oauth-clients`,
+          name: 'OAuth Apps',
+          key: 'oauth-apps',
+          url: `/project/${ref}/auth/oauth-apps`,
           items: [],
         },
       ],
